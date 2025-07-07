b9f9b0e028385ddf2871c0276b05f5cfca01841c
storm-beyndtech <khameleonstormstorm@gmail.com>
2025-07-07 11:43:57 +0100

    trust

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ import TransactionsSection from "../components/Home/TransactionsSection";
 import MissionVision from "../components/About/MissionVison";
 import StepsSection from "@/components/Home/StepsSection";
 import InvestmentOverviewSection from "@/components/Home/InvestmentOverviewSection";
-import Reviews from "@/components/Reviews";
+// import Reviews from "@/components/Reviews";
 import { useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
 
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
 			<UnifiedBusinessSections />
 			<InvestorCard />
 			<TransactionsSection />
-			<Reviews />
+			{/* <Reviews /> */}
 			<Cta />
 		</div>
 	);
