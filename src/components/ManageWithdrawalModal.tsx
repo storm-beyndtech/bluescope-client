3e0ed5eca54c8f1fd48118d736ba6843e8822b2c
storm-beyndtech <khameleonstorm@gmail.com>
2025-08-01 14:18:00 +0100

    plan

--- src/components/ManageWithdrawalModal.tsx
+++ src/components/ManageWithdrawalModal.tsx
@@ -124,27 +124,28 @@ export default function ManageWithdrawalModal({
 							<p className={`text-sm font-medium ${withdrawal.status} truncate`}>{withdrawal.status}</p>
 						</div>
 
+						{error && <Alert type="danger" message={error} />}
+						{success && <Alert type="success" message={success as any} />}
+
 						{withdrawal.status === "pending" && (
 							<div className="flex gap-5">
-								<a
-									href="#"
+								<button
+                  disabled={successLoading}
 									onClick={() => startUpdate("approved")}
 									className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 								>
 									{successLoading ? "Loading..." : "Approve"}
-								</a>
+								</button>
 
-								<a
-									href="#"
+                <button
+                  disabled={failedLoading}
 									onClick={() => startUpdate("rejected")}
 									className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
 								>
 									{failedLoading ? "Loading..." : "Reject"}
-								</a>
+								</button>
 							</div>
 						)}
-						{error && <Alert type="danger" message={error} />}
-						{success && <Alert type="success" message={success as any} />}
 					</form>
 				</div>
 			</div>
